351a59ea60eeb5607eb5b0871de4486606cbdfa0
Tompa <istuktomislav25@gmail.com>
2022-08-20 17:59:40 +0200

    adding react app

---
+++ client/src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">Sportski termini</header>
+    </div>
+  );
+}
+
+export default App;
